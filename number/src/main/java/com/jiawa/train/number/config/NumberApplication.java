d0447fb3d2bd1ba73cba116c28565be3be491c7d
甲蛙 <userEmail>
2025-08-19 15:25:22 +0800

    number： 新增banner.txt 新增启动类log 新增context方便后续路由转发

--- number/src/main/java/com/jiawa/train/number/config/NumberApplication.java
+++ number/src/main/java/com/jiawa/train/number/config/NumberApplication.java
@@ -1,15 +1,22 @@
 package com.jiawa.train.number.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class NumberApplication {
+    //日志
+    private static final Logger LOG = LoggerFactory.getLogger(NumberApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(NumberApplication.class, args);
+    public static void main(String[] args){
+        SpringApplication app=new SpringApplication(NumberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("测试地址：\thttp://127.0.0.1:{}/hello",env.getProperty("server.port"));
     }
-
 }
